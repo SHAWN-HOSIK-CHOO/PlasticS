261015ae3e18e87c0ec154573e6f94809dcffef0
SHAWN_C <67544399+SHAWN-HOSIK-CHOO@users.noreply.github.com>
2024-04-14 18:09:04 +0900

    14/04/2024
    Finished Propotype no.1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class GameManager : MonoBehaviour
 {
@@ -14,10 +16,12 @@ public class GameManager : MonoBehaviour
 
    public TextMeshProUGUI roundText;
    public TextMeshProUGUI specialScore;
+   public TextMeshProUGUI displayTotalScoreText;
 
    private void Start()
    {
       specialScore.gameObject.SetActive(false);
+      displayTotalScoreText.gameObject.SetActive(false);
    }
 
    private void Update()
@@ -27,6 +31,11 @@ public class GameManager : MonoBehaviour
       if (sExtraScoreCount >= 2)
       {
          StartCoroutine(ExtraScoreTMP());
+      }
+
+      if (sCurRoundCount > sMaxRoundCount)
+      {
+         StartCoroutine(ToEndScene());
       }
    }
 
@@ -38,5 +47,16 @@ public class GameManager : MonoBehaviour
       specialScore.gameObject.SetActive(true);
       yield return new WaitForSeconds(1.5f);
       specialScore.gameObject.SetActive(false);
+   }
+
+   IEnumerator ToEndScene()
+   {
+      sCurRoundCount             = 0;
+      displayTotalScoreText.text = "Total Score : " + sScore.ToString() + " !!";
+      sScore                     = 0;
+      displayTotalScoreText.gameObject.SetActive(true);
+      yield return new WaitForSeconds(2.0f);
+      displayTotalScoreText.gameObject.SetActive(false);
+      SceneManager.LoadScene("EndScene");
    }
 }
